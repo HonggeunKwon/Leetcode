c9918bd8afe96f05ef1bff4ff6be994a5c98fcc6
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2024-01-03 12:45:12 +0900

    Time: 2715 ms (34.19%), Space: 17.5 MB (24.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,21 +1,18 @@
 class Solution:
+    def isAnagram(self, s:str, left: int, right: int) -> bool:
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+    
     def longestPalindrome(self, s: str) -> str:
-        def expand(left: int, right: int) -> str:
-            s_len = len(s)
-            while left >= 0 and right < s_len and s[left] == s[right]:
-                left -= 1
-                right += 1
-            
-            return s[left + 1: right]
         
-        if len(s) < 2 or s == s[::-1]:
-            return s
+        str_len = len(s)
         
-        result = ''
-        
-        for i in range(len(s)):
-            result = max(result,
-                        expand(i, i + 2),
-                        expand(i, i + 1),
-                        key=len)
-        return result
+        for length in range(str_len, 0, -1):
+            for start_idx in range(0, str_len - length + 1):
+                if self.isAnagram(s, start_idx, start_idx + length - 1):
+                    return s[start_idx:(start_idx + length)]
+        return ''
