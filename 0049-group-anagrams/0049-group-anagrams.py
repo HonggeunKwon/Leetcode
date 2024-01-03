0d331534a696d84a2d52f0da88a116df7882707b
redcarrot0428 <108642272+HonggeunKwon@users.noreply.github.com>
2024-01-03 12:31:51 +0900

    Time: 91 ms (83.30%), Space: 20.4 MB (50.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,13 +2,15 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_dict = collections.defaultdict(list)
         
+        strs.sort()
         for str in strs:
             key = sorted(str)
             anagram_dict[''.join(key)].append(str)
             
-#       CASE #1
         answer = list(anagram_dict.values())
         answer.sort(key=len)
 
         return answer
-        # return list(anagram_dict.values()).sort(key=len)
+    
+#         Question 
+#         return list(anagram_dict.values()).sort(key=len)
