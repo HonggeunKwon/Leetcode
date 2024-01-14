7eabe5b031a2c663f6c3cb7494aee6e7633a1ac0
redcarrot0428 <108642272+dev-khg@users.noreply.github.com>
2024-01-14 14:21:51 +0900

    Time: 19 ms (28.40%), Space: 13.4 MB (49.83%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,9 +1,13 @@
-class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        counter = collections.Counter(stones)
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
         answer = 0
         
-        for jewel in jewels:
-            answer += counter[jewel]
+        jewel_set = set(jewels)
+        
+        for c in stones:
+            if c in jewel_set:
+                answer += 1
         
         return answer
+                
+        
