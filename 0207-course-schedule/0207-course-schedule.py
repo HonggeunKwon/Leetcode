769e309ab5681256062c5159a183e9a9325ccb2e
redcarrot0428 <108642272+dev-khg@users.noreply.github.com>
2024-03-09 00:06:57 +0900

    Time: 90 ms (59.53%), Space: 18.4 MB (46.30%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        course_dict = collections.defaultdict(list)
+        courses = collections.defaultdict(list)
         
-        for course, pre in prerequisites:
-            course_dict[pre].append(course)
-        
+        for next_course, pre_course in prerequisites:
+            courses[pre_course].append(next_course)
+            
         visited = {}
         
-        def cycle(node, tracker, visited) -> bool:
+        def is_cycle(node: int, tracker, visited) -> bool:
             tracker[node] = True
             visited[node] = True
-            for next_course in course_dict[node]:
-                if next_course not in visited and cycle(next_course, tracker, visited):
+            
+            for next_course in courses[node]:
+                if next_course not in visited and is_cycle(next_course, tracker, visited):
                     return True
                 elif next_course in tracker:
                     return True
             tracker.pop(node)
             return False
         
-        for course in range(numCourses):
-            if course not in visited and cycle(course, {}, visited):
+        for i in range(numCourses):
+            if i not in visited and is_cycle(i, {}, visited):
                 return False
         
         return True
-    
