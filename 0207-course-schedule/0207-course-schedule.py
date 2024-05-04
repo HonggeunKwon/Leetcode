b2a3e7cbd494b7a89ffbd1fead3e2eac5e084eed
redcarrot0428 <108642272+dev-khg@users.noreply.github.com>
2024-05-04 16:11:53 +0900

    Time: 97 ms (21.25%), Space: 18.4 MB (53.78%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = collections.defaultdict(list)
+        courses = collections.defaultdict(list)
         
-        for a, b in prerequisites:
-            graph[b].append(a)
-        
-        visited = {}
-        
-        def is_cycle(node, visited, tracker):            
+        def is_cycle(node, visited, tracker):
             visited[node] = True
             tracker[node] = True
             
-            for next_node in graph[node]:
-                if next_node not in visited and is_cycle(next_node, visited, tracker):
+            for i in courses[node]:
+                if i not in visited and is_cycle(i, visited, tracker):
                     return True
-                elif next_node in tracker:
+                elif i in tracker:
                     return True
-            tracker.pop(node)            
+            tracker.pop(node)
             return False
-            
+        
+        
+        for after, before in prerequisites:
+            courses[before].append(after)
+        
+        visited = {}
         
         for i in range(numCourses):
             if i not in visited and is_cycle(i, visited, {}):
